f44c1ea1990aed28e3385be1e871db40aeab531f
Sandeshsbsandesh <142797162+Sandeshsbsandesh@users.noreply.github.com>
2023-08-28 13:24:04 +0530

    Add files via upload

---
+++ CompareRepo.cs
@@ -0,0 +1,85 @@
+﻿using Dapper;
+using MySqlConnector;
+using Npgsql;
+using System.Data;
+using WebApplication6.Data;
+using WebApplication6.Models;
+
+namespace WebApplication6.Repository
+
+{
+    public class Compare
+    {
+       
+            private readonly DapperDbContext _dbContext;
+            private readonly string _connectionString;
+            public Compare(DapperDbContext dbcontext, IConfiguration configuration)
+        { 
+            string connectionString1 = "DefaultConnection";
+            string connectionString2 = "DefaultConnection2";
+
+            using (var connection = new NpgsqlConnection(connectionString1))
+            using (var connection2 = new NpgsqlConnection(connectionString2))
+            {
+                connection.Open();
+                connection2.Open();
+                var table1 = connection.Query<string>("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';").AsList();
+                var table2 = connection2.Query<string>("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';").AsList();
+
+
+                var tablesDifference = new List<string>();
+
+                foreach (var table in table1)
+                {
+                    if (!table2.Contains(table))
+                    {
+                        tablesDifference.Add(table);
+                    }
+                }
+
+                foreach (var table in table2)
+                {
+                    if (!table1.Contains(table))
+                    {
+                        tablesDifference.Add(table);
+                    }
+                }
+                Console.WriteLine("Tables with differences:");
+                foreach (var table in tablesDifference)
+                {
+                    Console.WriteLine(table);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Startup.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.OpenApi.Models;
+using WebApplication6.Data;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Options;
+using WebApplication6.Repository;
+using WebApplication6.Repositoryy;
+
+namespace WebApplication6
+{
+    public class Startup
+
+
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public class ConnectionStringsConfig
+        {
+            public string DefaultConnection { get; set; }
+
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+
+        {
+            services.AddDbContext<EmployeeDbcontext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<EmployeeDbcontext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection2")));
+            services.AddControllers();
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole(); // Use the console logger provider
+            });
+
+            services.AddSingleton<DapperDbContext>();
+            services.AddScoped<IDepartmentRepo,DepartmentRepo>();
+
+            services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Resolve Employee", Version = "v1" });
+
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+            });
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        
+
+            app.UseCors("AllowAll");
+        }
+
+    }
+}
